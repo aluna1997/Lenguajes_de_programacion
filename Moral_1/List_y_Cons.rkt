75c18bc87bff96f23ae077b96323c4f04ff792e8
aluna1997 <aluna1997@ciencias.unam.mx>
2018-08-27 00:49:05 -0500

    Lenguajes

---
+++ Moral_1/List_y_Cons.rkt
@@ -0,0 +1,5 @@
+#lang plai
+;CON LIST
+(list 4(list (list "Adrián" 0)(list "Juan"1)(list "Luis" 2))3(list "Daniel" 5))
+;CON CONS
+(cons 4 (cons (cons (cons "Adrián" (cons 0 empty)) (cons (cons "Juan" (cons 1 empty)) (cons (cons "Luis" (cons 2 empty)) empty))) (cons 3 (cons (cons "Daniel" (cons 5 empty)) empty))))
